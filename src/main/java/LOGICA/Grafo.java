d6701a1aa050314a2a22a9e8c063e634dba78747
delog <delog@HPPAVILION-DFGM>
2023-06-24 17:20:33 -0500

    commit areglado

--- src/main/java/LOGICA/Grafo.java
+++ src/main/java/LOGICA/Grafo.java
@@ -1,58 +1,58 @@
 package LOGICA;
 
-import java.util.*;
+// import java.util.*;
 
-public class Grafo<T> {
-    private Map<T, LinkedList<T>> adyacencia;
+// public class Grafo<T> {
+//     private Map<T, LinkedList<T>> adyacencia;
 
-    public Grafo() {
-        adyacencia = new Map<>();
-    }
+//     public Grafo() {
+//         adyacencia = new Map<>();
+//     }
 
-    public void agregarVertice(T vertice) {
-        adyacencia.put(vertice, new LinkedList<T>());
-    }
+//     public void agregarVertice(T vertice) {
+//         adyacencia.put(vertice, new LinkedList<T>());
+//     }
 
-    public void agregarArista(T origen, T destino) {
-        if (!adyacencia.containsKey(origen))
-            agregarVertice(origen);
+//     public void agregarArista(T origen, T destino) {
+//         if (!adyacencia.containsKey(origen))
+//             agregarVertice(origen);
 
-        if (!adyacencia.containsKey(destino))
-            agregarVertice(destino);
+//         if (!adyacencia.containsKey(destino))
+//             agregarVertice(destino);
 
-        adyacencia.get(origen).pushBack(destino);
-        adyacencia.get(destino).pushBack(origen);
-    }
+//         adyacencia.get(origen).pushBack(destino);
+//         adyacencia.get(destino).pushBack(origen);
+//     }
 
-    public Set<T> obtenerVertices() {
-        return adyacencia.keySet();
-    }
+//     public Set<T> obtenerVertices() {
+//         return adyacencia.keySet();
+//     }
 
-    public LinkedList<T> obtenerVecinos(T vertice) {
-        if (adyacencia.containsKey(vertice))
-            return adyacencia.get(vertice);
+//     public LinkedList<T> obtenerVecinos(T vertice) {
+//         if (adyacencia.containsKey(vertice))
+//             return adyacencia.get(vertice);
 
-        return new LinkedList<T>();
-    }
+//         return new LinkedList<T>();
+//     }
 
-    public boolean contieneVertice(T vertice) {
-        return adyacencia.containsKey(vertice);
-    }
+//     public boolean contieneVertice(T vertice) {
+//         return adyacencia.containsKey(vertice);
+//     }
 
-    public boolean sonAdyacentes(T vertice1, T vertice2) {
-        boolean adyasentes = false;
-        if (!adyacencia.containsKey(vertice1) || !adyacencia.containsKey(vertice2))
-            return adyasentes;
+//     public boolean sonAdyacentes(T vertice1, T vertice2) {
+//         boolean adyasentes = false;
+//         if (!adyacencia.containsKey(vertice1) || !adyacencia.containsKey(vertice2))
+//             return adyasentes;
 
-        try {
-            for (T i : adyacencia.get(vertice1).getAll()) {
-                if (i == vertice2) {
-                    adyasentes = true;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return adyasentes;
-    }
-}
+//         try {
+//             for (T i : adyacencia.get(vertice1).getAll()) {
+//                 if (i == vertice2) {
+//                     adyasentes = true;
+//                 }
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//         return adyasentes;
+//     }
+// }
--- src/main/java/LOGICA/Map.java
+++ src/main/java/LOGICA/Map.java
@@ -1,62 +1,62 @@
 package LOGICA;
 
 
-public class Map<K, V> {
-    private ArrayList<K> llaves;
-    private ArrayList<V> valores;
+// public class Map<K, V> {
+//     private ArrayList<K> llaves;
+//     private ArrayList<V> valores;
 
-    public Map() {
-        llaves = new ArrayList<>();
-        valores = new ArrayList<>();
-    }
+//     public Map() {
+//         llaves = new ArrayList<>();
+//         valores = new ArrayList<>();
+//     }
 
-    public void agregar(K llave, V valor) {
-        int indice = llaves.indexOf(llave);
+//     public void agregar(K llave, V valor) {
+//         int indice = llaves.indexOf(llave);
 
-        if (indice != -1) {
-            valores.set(indice, valor);
-        } else {
-            llaves.add(llave);
-            valores.add(valor);
-        }
-    }
+//         if (indice != -1) {
+//             valores.set(indice, valor);
+//         } else {
+//             llaves.add(llave);
+//             valores.add(valor);
+//         }
+//     }
 
-    public V obtener(K llave) {
-        int indice = llaves.indexOf(llave);
+//     public V obtener(K llave) {
+//         int indice = llaves.indexOf(llave);
 
-        if (indice != -1) {
-            return valores.get(indice);
-        }
+//         if (indice != -1) {
+//             return valores.get(indice);
+//         }
 
-        return null;
-    }
+//         return null;
+//     }
 
-    public void eliminar(K llave) {
-        int indice = llaves.indexOf(llave);
+//     public void eliminar(K llave) {
+//         int indice = llaves.indexOf(llave);
 
-        if (indice != -1) {
-            llaves.remove(indice);
-            valores.remove(indice);
-        }
-    }
+//         if (indice != -1) {
+//             llaves.remove(indice);
+//             valores.remove(indice);
+//         }
+//     }
 
-    public List<K> obtenerLlaves() {
-        return new ArrayList<>(llaves);
-    }
+//     public List<K> obtenerLlaves() {
+//         return new ArrayList<>(llaves);
+//     }
 
-    public List<V> obtenerValores() {
-        return new ArrayList<>(valores);
-    }
+//     public List<V> obtenerValores() {
+//         return new ArrayList<>(valores);
+//     }
 
-    public boolean contieneLlave(K llave) {
-        return llaves.contains(llave);
-    }
+//     public boolean contieneLlave(K llave) {
+//         return llaves.contains(llave);
+//     }
 
-    public boolean contieneValor(V valor) {
-        return valores.contains(valor);
-    }
+//     public boolean contieneValor(V valor) {
+//         return valores.contains(valor);
+//     }
 
-    public int tamaño() {
-        return llaves.size();
-    }
-}
+//     public int tamaño() {
+//         return llaves.size();
+//     }
+// }
