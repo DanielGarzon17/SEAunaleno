b5a62f5ca05ba1d1d5e98a142c03625c7a08c7b6
Cristian <crgarciapa@unal.edu.co>
2023-06-24 14:55:36 -0500

    commit mainremote

--- src/main/java/LOGICA/Grafo.java
+++ src/main/java/LOGICA/Grafo.java
@@ -1,7 +1,60 @@
 package LOGICA;
 
-import java.util.Dictionary;
-public class Grafo <k, v>{
-    public Dictionary<k,v> lista;
-    
+import java.util.*;
+
+public class Grafo<T> {
+    private Map<T, LinkedList<T>> adyacencia;
+
+    public Grafo() {
+        adyacencia = new HashMap<>();
+    }
+
+    public void agregarVertice(T vertice) {
+        adyacencia.put(vertice, new LinkedList<T>());
+    }
+
+    public void agregarArista(T origen, T destino) {
+        if (!adyacencia.containsKey(origen))
+            agregarVertice(origen);
+
+        if (!adyacencia.containsKey(destino))
+            agregarVertice(destino);
+
+        adyacencia.get(origen).pushBack(destino);
+        ;
+        adyacencia.get(destino).pushBack(origen);
+        ; // Si el grafo es no dirigido
+    }
+
+    public Set<T> obtenerVertices() {
+        return adyacencia.keySet();
+    }
+
+    public LinkedList<T> obtenerVecinos(T vertice) {
+        if (adyacencia.containsKey(vertice))
+            return adyacencia.get(vertice);
+
+        return new LinkedList<T>();
+    }
+
+    public boolean contieneVertice(T vertice) {
+        return adyacencia.containsKey(vertice);
+    }
+
+    public boolean sonAdyacentes(T vertice1, T vertice2) {
+        boolean adyasentes = false;
+        if (!adyacencia.containsKey(vertice1) || !adyacencia.containsKey(vertice2))
+            return adyasentes;
+
+        try {
+            for (T i : adyacencia.get(vertice1).getAll()) {
+                if (i == vertice2) {
+                    adyasentes = true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return adyasentes;
+    }
 }
